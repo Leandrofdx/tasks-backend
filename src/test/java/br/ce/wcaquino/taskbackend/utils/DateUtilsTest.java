52a6c4251244227f414c99c749a845cf17f94e55
Leandro <leandro@MacBook-Air-de-Mac.local>
2021-03-04 15:36:01 -0300

    Adicionando testes unitarios

--- src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -10,8 +10,7 @@ public class DateUtilsTest {
 	@Test
 	public void deveRetornarTrueParaDatasFuturas() {
 		LocalDate data = LocalDate.of(2030, 01, 01);
-//		Assert.assertTrue(DateUtils.isEqualOrFutureDate(data));
-		Assert.assertFalse(DateUtils.isEqualOrFutureDate(data));
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(data));
 	}
 	
 	@Test
